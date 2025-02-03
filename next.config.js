e94de59e956039c0b4845b010ea13cbdb3ebf7b4
Julian Hilgenberg <julian@hilgenberg.cc>
2025-02-03 13:45:08 +0100

    fix?

--- next.config.js
+++ next.config.js
@@ -10,6 +10,8 @@ const nextConfig = {
     config.resolve.fallback = {
       ...config.resolve.fallback,
       "crypto": require.resolve("crypto-browserify"),
+      "stream": require.resolve("stream-browserify"),
+      "buffer": require.resolve("buffer/"),
     }
     return config
   },
